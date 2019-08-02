fd6db0d6c05f9ada8c3c23cf6266b55894a1f38c
ioriwellings <wellingsok@hotmail.com>
2019-08-02 15:30:54 +0800

    commit

---
+++ IBLL/ISysFieldHander.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Langben.IBLL
+{
+    public interface ISysFieldHander
+    {
+        string GetMyTextsById(string id);
+        global::System.Collections.Generic.List<global::Langben.DAL.SysField> GetSysField(string table, string colum);
+        global::System.Collections.Generic.List<global::Langben.DAL.SysField> GetSysField(string table, string colum, string parentMyTexts);
+        global::System.Collections.Generic.List<global::Langben.DAL.SysField> GetSysFieldByParent(string id, string parentid, string value);
+        global::System.Collections.Generic.List<global::Langben.DAL.SysField> GetSysFieldByParentId(string id);
+    }
+}
